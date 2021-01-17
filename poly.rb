0dbb6b9e37a4e44a7cbcb6b17031e3e29a4b25bd
Marcelo <marcelo.sanzoni@gmail.com>
2021-01-17 16:18:36 -0300

    Polymorphismo

---
+++ poly.rb
@@ -0,0 +1,37 @@
+class Instrumento
+    def escrever
+        puts 'Escrevendo'
+    end
+end
+
+class Teclado < Instrumento
+    def escrever
+        puts 'Desenhar'
+        super
+    end
+end
+
+class Lapis < Instrumento
+    def escrever
+        puts 'Escrevendo à Lápis'
+    end
+end
+
+class Caneta < Instrumento
+    def escrever
+        puts 'Escrevendo à Caneta'
+    end
+end
+
+teclado = Teclado.new
+lapis = Lapis.new
+caneta = Caneta.new
+
+puts "Lapis: "
+lapis.escrever
+puts "Caneta: "
+caneta.escrever
+puts "Teclado: "
+teclado.escrever
+
+
