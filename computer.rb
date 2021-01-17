51da241b1bd77e5c99c43dcb8c93b01790835c26
Marcelo <marcelo.sanzoni@gmail.com>
2021-01-17 15:01:01 -0300

    Aula anterior finalizada

--- computer.rb
+++ computer.rb
@@ -1,6 +1,31 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery whit: :exception
 
+  def index
+    @file = file.scoped
+  end
+
+  def show
+    @file = file.find(params[:id])
+  end
+
+  def create
+    @file = file.new(params[:file])
+  end
+
+  def update
+    @file = file.find(params[:id])
+  end
+
+  def edit
+    @file = file.find(params[:id])
+  end
+
+  def destroy
+    file = file.find(params[:id])
+  end
+
+
     console.onebitcode('VS Code Curso');
-    puts '\o/'
+    puts 'Deu certo!!'
 end 
